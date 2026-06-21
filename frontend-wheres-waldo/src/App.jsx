6f49d3cf86443ce3919a7b1a470994d43768b45e
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-06-21 09:27:29 +0000

    Initial commit

---
+++ frontend-wheres-waldo/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Where's Waldo</h1>
+      <img src="https://wallpapercave.com/wp/wp7156937.jpg" alt="where's waldo image" />
+    </div>
+  )
+}
