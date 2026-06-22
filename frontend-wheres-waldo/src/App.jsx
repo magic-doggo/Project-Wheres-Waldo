fdd667098d4ef190fb3e0c753446c6be73adb646
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-06-22 11:08:15 +0000

    Retrieve the coordinates of the cursor clicks on the image

--- frontend-wheres-waldo/src/App.jsx
+++ frontend-wheres-waldo/src/App.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 export default function App() {
+  const [clickCoords, setClickCoords] = useState(null);
+  function handleImageClick(e) {
+    const img = e.currentTarget;
+    const rect = img.getBoundingClientRect();
+    //x coord = (px from left of browser window to click - px from left browser window to left of image)/image width on screen = px from left of image to click/width(to get%)
+    const x = (e.clientX - rect.left) / rect.width;  
+    const y = (e.clientY - rect.top) / rect.height;
+    setClickCoords({x,y})
+    console.log({ x, y });
+    displayAvailableCharacters()
+  }
+
+  function displayAvailableCharacters() {
+    //check db and return only characters not found
+    //pop up window with image/names of these characters
+    //add for each image: onClick guessCharacter(clickCoords, characterClickedOn)
+  }
+  function guessCharacter(chosenCharacter) {
+    //check db if that character exists on clickCoords
+  }
   return (
     <div>
       <h1>Where's Waldo</h1>
-      <img src="https://wallpapercave.com/wp/wp7156937.jpg" alt="where's waldo image" />
+      <img
+        src="https://wallpapercave.com/wp/wp7156937.jpg"
+        alt="where's waldo image"
+        onClick={handleImageClick}
+        style={{ maxWidth: '100%' }}
+      />
     </div>
   )
 }
