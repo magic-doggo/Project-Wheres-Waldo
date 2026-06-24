1f822228724b6618f40a37c9d8f616b9b83aa9fd
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-06-24 07:07:22 +0000

    Display notFoundCharacters when user clicks on any coordinates choosing a character hides the notFoundCharacters tab again

--- frontend-wheres-waldo/src/App.jsx
+++ frontend-wheres-waldo/src/App.jsx
@@ -2,34 +2,60 @@ import { useState } from "react";
 
 export default function App() {
   const [clickCoords, setClickCoords] = useState(null);
+  const [characters, setCharacters] = useState([
+    {id: 1, name: "waldo", image: "https://res.cloudinary.com/magicdoggo/image/private/s--lefRfJmZ--/v1775204755/git1.jpg.jpg", coordinates: { x: 0.5, y: 0.7 } },
+    {id: 2, name: "maldo", image: "https://res.cloudinary.com/magicdoggo/image/private/s--lefRfJmZ--/v1775204755/git1.jpg.jpg", coordinates: { x: 0.4, y: 0.2 } }]); // pull from db once backend is created
+  const [foundCharacterIds, setFoundCharacterIds] = useState([]);
+
+
+  const notFoundCharacters = characters.filter((char) => !foundCharacterIds.includes(char.id))
+
   function handleImageClick(e) {
     const img = e.currentTarget;
     const rect = img.getBoundingClientRect();
     //x coord = (px from left of browser window to click - px from left browser window to left of image)/image width on screen = px from left of image to click/width(to get%)
-    const x = (e.clientX - rect.left) / rect.width;  
+    const x = (e.clientX - rect.left) / rect.width;
     const y = (e.clientY - rect.top) / rect.height;
-    setClickCoords({x,y})
+    setClickCoords({ x, y })
     console.log({ x, y });
-    displayAvailableCharacters()
+    // displayAvailableCharacters() //just jsx the notFoundCharacters tab open when clickCoords are set
   }
 
-  function displayAvailableCharacters() {
-    //check db and return only characters not found
-    //pop up window with image/names of these characters
-    //add for each image: onClick guessCharacter(clickCoords, characterClickedOn)
-  }
   function guessCharacter(chosenCharacter) {
     //check db if that character exists on clickCoords
+    setClickCoords(null);
   }
   return (
     <div>
       <h1>Where's Waldo</h1>
-      <img
-        src="https://wallpapercave.com/wp/wp7156937.jpg"
-        alt="where's waldo image"
-        onClick={handleImageClick}
-        style={{ maxWidth: '100%' }}
-      />
+      <div className="image-wrapper">
+
+        <img
+          src="https://wallpapercave.com/wp/wp7156937.jpg"
+          alt="where's waldo image"
+          onClick={handleImageClick}
+          style={{ maxWidth: '100%' }}
+        />
+        {clickCoords && (
+          <div
+            style={{
+              position: 'absolute',
+              left: `${clickCoords.x * 100}%`,
+              top: `${clickCoords.y * 100}%`,
+            }}>
+            {notFoundCharacters.map((char) => (
+              <button key={char.id} type="button" onClick={() => guessCharacter(char.id)}>
+                <img src={char.image} alt={char.name}
+                  style={{
+                    width: '48px',
+                    height: '48px',
+                  }} />
+                <div>{char.name}</div>
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
