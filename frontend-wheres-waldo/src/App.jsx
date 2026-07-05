4ae8b72dc3d92a44b881447a8abc33fc96d413b9
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2026-07-05 09:42:01 +0000

    Add option to start game and api call BE to guess characters

--- frontend-wheres-waldo/src/App.jsx
+++ frontend-wheres-waldo/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export default function App() {
   const [clickCoords, setClickCoords] = useState(null);
-  const [characters, setCharacters] = useState([
-    { id: 1, name: "waldo", image: "https://res.cloudinary.com/magicdoggo/image/private/s--lefRfJmZ--/v1775204755/git1.jpg.jpg", coordinates: { x: 0.5, y: 0.7 } },
-    { id: 2, name: "maldo", image: "https://res.cloudinary.com/magicdoggo/image/private/s--lefRfJmZ--/v1775204755/git1.jpg.jpg", coordinates: { x: 0.4, y: 0.2 } }]); // pull from db once backend is created
+  // const [characters, setCharacters] = useState([
+  //   { id: 1, name: "waldo", image: "https://res.cloudinary.com/magicdoggo/image/private/s--lefRfJmZ--/v1775204755/git1.jpg.jpg", coordinates: { x: 0.5, y: 0.7 } },
+  //   { id: 2, name: "maldo", image: "https://res.cloudinary.com/magicdoggo/image/private/s--lefRfJmZ--/v1775204755/git1.jpg.jpg", coordinates: { x: 0.4, y: 0.2 } }]); // pull from db once backend is created
+  const [characters, setCharacters] = useState([]);
+  const [gameId, setGameId] = useState(null);
   const [foundCharacterIds, setFoundCharacterIds] = useState([]);
 
 
   const notFoundCharacters = characters.filter((char) => !foundCharacterIds.includes(char.id))
+
+  async function startGame() {
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/games`, {
+        method: "POST",
+      });
+      const data = await response.json();
+      setGameId(data.gameId);
+      setCharacters(data.characters);
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   function handleImageClick(e) { //only runs if clickCoords null. otherwise the click does not go on the image, it goes on "fixed" position backdrop
     e.stopPropagation();
@@ -22,16 +39,91 @@ export default function App() {
     // displayAvailableCharacters() //just jsx the notFoundCharacters tab open when clickCoords are set
   }
 
-  function guessCharacter(chosenCharacter) {
+  async function guessCharacter(chosenCharacterId) {
     //check db if that character exists on clickCoords
-    setClickCoords(null);
+    if (!clickCoords || !gameId) return;
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/games/${gameId}/guesses`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          characterId: chosenCharacterId,
+          clickX: clickCoords.x,
+          clickY: clickCoords.y
+        })
+      });
+      const data = await response.json();
+      console.log("data:", data);
+
+      if (!response.ok) {
+        console.log("error: ", data.error);
+        alert(data.error || data.message);
+      } else {
+        console.log(data.message);
+        setFoundCharacterIds((prev) => [...prev, chosenCharacterId]);
+        if (data.gameOver) {
+          console.log(`game won in ${data.duration} milliseconds`)
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setClickCoords(null); //reset coords after a guess
+    }
+    // setClickCoords(null);
   }
   return (
     <div>
       <h1>Where's Waldo</h1>
+      {!gameId ? (
+        <button onClick={startGame}>Start Game</button>
+      ) : (
+        <div className="image-wrapper" style={{ position: 'relative' }}>
+          <img
+            // src="https://wallpapercave.com/wp/wp7156937.jpg"
+            src="wheres-waldo.webp"
+            alt="where's waldo image"
+            onClick={handleImageClick}
+            style={{ maxWidth: '100%' }}
+          />
+          {clickCoords && (
+            <div className="guess-character-container">
+              <div
+                style={{ position: 'fixed', inset: 0, zIndex: 5 }}  //covers entire screen while clickCoords are not null, to remove guess character tab on clicking anywhere
+                onClick={() => setClickCoords(null)}
+              />
+
+              <div
+                style={{
+                  position: 'absolute',
+                  left: `${clickCoords.x * 100}%`,
+                  top: `${clickCoords.y * 100}%`,
+                  width: 'max-content',
+                  zIndex: 10,
+                }}>
+                {notFoundCharacters.map((char) => (
+                  <button key={char.id} type="button" onClick={() => {guessCharacter(char.id), console.log(notFoundCharacters)}}>
+                    <img src={char.iconUrl} alt={char.name}
+                      style={{
+                        width: '48px',
+                        height: '48px',
+                      }} />
+                    <div>{char.name}</div>
+                  </button>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+
+
       <div className="image-wrapper" style={{ position: 'relative' }}>
 
-        <img
+        {/* <img
           // src="https://wallpapercave.com/wp/wp7156937.jpg"
           src="wheres-waldo.webp"
           alt="where's waldo image"
@@ -65,7 +157,7 @@ export default function App() {
               ))}
             </div>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   )
